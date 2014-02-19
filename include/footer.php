6b893f7178e43051c5fb69984ac40c7143ac9986
Marcus Antônio <delphi.marcus@gmail.com>
2014-02-19 19:18:56 -0300

    Criação da pasta js dentro da assets Inserção do Jquery Criação da Pasta Include Criação dos arquivos header.php e footer.php Criação do arquivo index.php

---
+++ include/footer.php
@@ -0,0 +1,7 @@
+    <!-- Fim conteudo  -->
+    <!-- Inclusão jQuery -->
+    <script src="assets/js/jquery.js"></script>
+    <!-- Inclusão do bootstrap -->
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require_once './include/header.php'; ?>
+<!--Fim da Inclusão do Header na Pagina-->
+
+<!--Incluindo o Menu na Pagina-->
+<?php ?>
+<!--Fim da Inclusão do Menu na Pagina-->
+
+<!-- Incluido o Conteudo na Pagina -->
+<?php ?>
+<!-- Fim da Inclusão do Conteudo na Pagina -->
+
+<!--Incluindo o Footer na Pagina-->
+<?php require_once './include/footer.php'; ?>
+<!--Fim da Inclusão do Footer na Pagina-->
+  
