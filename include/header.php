b71987d607c8bb60a8e94c7e1be09566fa4c7feb
CarlosROliveira <carlos.rc.oliveira@gmail.com>
2014-02-21 19:14:04 -0300

    Criação da pasta CSS e do arquivo estilo.css

--- include/header.php
+++ include/header.php
@@ -7,6 +7,7 @@
     <title>Sistema de Inscrição</title>
     <!-- Bootstrap -->
     <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/estilo.css" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
